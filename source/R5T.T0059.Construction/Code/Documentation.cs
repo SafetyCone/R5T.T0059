cc5119f0890b8fcc6d2ce5ea9b1abb8f61ee0b0a
davidcoats <david@getsafetycone.com>
2021-10-08 13:19:52 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0059.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0059.Construction
+{
+    /// <summary>
+    /// Construction project for IVisualStudioSolutionFolderName extension method base and other bases Visual Studio internal concepts..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
